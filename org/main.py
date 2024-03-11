388bb6180e7dac0b287815bfb2effada96c673bf
nkarisa <nkarisa@us.ci.org>
2024-03-11 18:47:19 +0300

    Add orm models

--- org/main.py
+++ org/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
-from . import schemas, crud
+from . import schemas, crud, models
 from .routes import org, contact_us
+from .database import SessionLocal, engine
+from sqlalchemy.orm import Session
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
 
 @app.get("/", tags=['Home'])
 async def root():
