e48d887ea78312c4c4a1d9211e532de64a7faf5a
nkarisa <nkarisa@us.ci.org>
2024-03-20 04:09:28 +0300

    Updated utils

---
+++ org/organisation/schemas/general_schemas.py
@@ -0,0 +1,14 @@
+from fastapi import UploadFile
+from datetime import datetime, date
+from typing import Union, List
+from enum import Enum
+from uuid import UUID
+
+class ImageTypeFileExtensions(str, Enum):
+    png = "png"
+    jpg = "jpg"
+    jpeg = "jpeg"
+
+class DocTypeFileExtensions(str, Enum):
+    pdf = "pdf"
+    csv = "csv"
